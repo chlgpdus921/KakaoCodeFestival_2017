042edaf11e885622be9b1e90056c62922c2b56a8
chlgpdus921 <chlgpdus921@naver.com>
2018-07-31 20:55:46 +0900

    A.java practice

---
+++ src/kakao/A.java
@@ -0,0 +1,16 @@
+package kakao;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i =0; i<n; i++){
+			System.out.println("Welcome to Kakao Code Festival!");
+		}
+	}
+
+}
--- src/myPackage/Problem2.java
+++ src/myPackage/Problem2.java
@@ -1,10 +1,27 @@
 package myPackage;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Problem2 {
+	static int m, n;
+	static int[][] city_map;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		m = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 
+		city_map = new int[m][n];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		/* Enter Input */
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				city_map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
 	}
 
 }
